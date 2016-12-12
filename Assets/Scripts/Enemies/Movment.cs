8a13ab5e02fe41373bab48e61bdf765c2ae7f61b
Erdal Erkilic <erdal.mybooks@hotmail.com>
2016-12-13 02:22:35 +0800

    So i got the animations all working at the correct time now, i have also allowed you to move our cave man enemy using the right and left arrow keys, just for the purposes of testing out some movment code and animation.

---
+++ Assets/Scripts/Enemies/Movment.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movment : MonoBehaviour
+{
+    public float speed = 8f, maxVelocity = 4f;
+
+    //The rigid body of what ever object we are effecting
+    private Rigidbody2D myBody;
+
+    //Use to controll animation
+    private Animator anim;
+
+    void Awake()
+    {
+        //Attaching the objects ridgidbody to this varable for future use
+        myBody = GetComponent<Rigidbody2D>();
+        //Attaching the objects Animator to this varable for future use
+        anim = GetComponent<Animator>();
+    }
+
+    //Called every 3-4 frames, good movments
+    void FixedUpdate()
+    {
+        PlayerMoveKeyboard();
+    }
+
+    //Move object by keyboard
+    void PlayerMoveKeyboard()
+    {
+        float forceX = 0f;
+        float vel = Mathf.Abs(myBody.velocity.x);
+
+        float h = Input.GetAxisRaw("Horizontal");
+
+        if (h > 0)
+        {
+            if (vel < maxVelocity)
+            {
+                forceX = speed;
+            }
+
+            Vector3 temp = transform.localScale;
+            temp.x = 1.3f;
+            transform.localScale = temp;
+
+            anim.SetBool("Walk", true);
+        }
+        else if (h < 0)
+        {
+            if (vel < maxVelocity)
+            {
+                forceX = -speed;
+            }
+
+            Vector3 temp = transform.localScale;
+            temp.x = -1f;
+            transform.localScale = temp;
+
+            anim.SetBool("Walk", true);
+        }
+        else
+        {
+            anim.SetBool("Walk", false);
+        }
+
+        myBody.AddForce(new Vector2(forceX, 0));
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+	
+	}
+}
