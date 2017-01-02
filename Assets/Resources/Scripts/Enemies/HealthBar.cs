a3798a4129ef2e1b32e69365d20a83c71698b349
Erdal Erkilic <erdal.mybooks@hotmail.com>
2017-01-02 11:46:36 +0800

    Made a few small changes to our prefabs again, seems some child elements were not on the correct sorting layer at the time, my bad. But more importently we now have a HealthBar class that as you could guess effects our enemy objects health bar, it scales the bar according to how much health the object has left, its great

---
+++ Assets/Resources/Scripts/Enemies/HealthBar.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour 
+{
+	public float maxHealth; //Store teh max health of this object
+	public float currentHealth; //Store the value of the health as it currently is
+	public float originalScale;
+
+	// Use this for initialization
+	void Start() 
+	{
+		SetCompoinents (); //Set our varable compoinents
+	}
+
+	//Set our varable compoinents
+	void SetCompoinents()
+	{
+		originalScale = gameObject.transform.localScale.x; //Get and store the size of the health bar
+		maxHealth = this.transform.parent.GetComponent<BasicStatsEnemies> ().health; //Get the health of the mainparent to determine the health of this object
+		currentHealth = maxHealth; //Current health starts off as the max health
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+		Vector3 tempScale = gameObject.transform.localScale; //Create a temporary scale for our healthbar and place the localScale of our health bar inside it
+		tempScale.x = currentHealth / maxHealth * originalScale; //Make the x position equal the percentage left over of health
+		gameObject.transform.localScale = tempScale; //Change the object itself to match our temporary object
+	}
+}
