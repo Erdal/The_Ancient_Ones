e6d9c31e6a524aa9d629a9ca20fb1c4b930bf11e
Erdal Erkilic <erdal.mybooks@hotmail.com>
2017-01-12 05:02:26 +0800

    I created a bunch of PlayerPrefs, i dont have them all yet but i created the basic ones i could think of for now. I also created getters and setters for all of them

--- Assets/Resources/Scripts/Game Preferences/GamePreferences.cs
+++ Assets/Resources/Scripts/Game Preferences/GamePreferences.cs
@@ -4,23 +4,87 @@ using System.Collections;
 public static class GamePreferences
 {
 	//Player Prefered Settings
-
-	public static string Music = "Music";
-
+	//
+	public static string BoolMusic = "BoolMusic";
 
 	//Upgrades
-
-	//f
-
+	//
+	public static string BasicDamageIncrease = "BasicDamageIncrease";
+	public static string BasicAttackSpeedIncrease = "BasicAttackSpeedIncrease";
+	public static string BasicRangeInscrease = "BasicRangeInscrease";
+	public static string BloodIncrease = "BloodIncrease";
+	public static string BloodXpIncrease = "BloodXpIncrease";
 
 	//Region_Map_Score
-
+	//
 	public static string RegionScore1_1 = "RegionScore1_1";
 
+	//Player Prefered Settings Getters and Setters
+	//
+	public static void SetBoolMusic(int yesNo)
+	{
+		PlayerPrefs.SetInt (GamePreferences.BoolMusic, yesNo);
+	}
 
+	public static int GetBoolMusic()
+	{
+		return PlayerPrefs.GetInt (GamePreferences.BoolMusic);
+	}
+
+	//Upgrade Getters and Setters
+	//
+	public static void SetBasicDamageIncrease(int level)
+	{
+		PlayerPrefs.SetInt (GamePreferences.BasicDamageIncrease, level);
+	}
+
+	public static int GetBasicDamageIncrease()
+	{
+		return PlayerPrefs.GetInt (GamePreferences.BasicDamageIncrease);
+	}
+
+	public static void SetBasicAttackSpeedIncrease(int level)
+	{
+		PlayerPrefs.SetInt (GamePreferences.BasicAttackSpeedIncrease, level);
+	}
+
+	public static int GetBasicAttackSpeedIncrease()
+	{
+		return PlayerPrefs.GetInt (GamePreferences.BasicAttackSpeedIncrease);
+	}
+
+	public static void SetBasicRangeInscrease(int level)
+	{
+		PlayerPrefs.SetInt (GamePreferences.BasicRangeInscrease, level);
+	}
+
+	public static int GetBasicRangeInscrease()
+	{
+		return PlayerPrefs.GetInt (GamePreferences.BasicRangeInscrease);
+	}
+
+	public static void SetBloodIncrease(int level)
+	{
+		PlayerPrefs.SetInt (GamePreferences.BloodIncrease, level);
+	}
+
+	public static int GetBloodIncrease()
+	{
+		return PlayerPrefs.GetInt (GamePreferences.BloodIncrease);
+	}
+
+	public static void SetBloodXpIncrease(int level)
+	{
+		PlayerPrefs.SetInt (GamePreferences.BloodXpIncrease, level);
+	}
+
+	public static int GetBloodXpIncrease()
+	{
+		return PlayerPrefs.GetInt (GamePreferences.BloodXpIncrease);
+	}
 
 	//Region_Map_Score Getters and Setters
-
+	//
 	public static void SetRegionScore1_1(float score)
 	{
 		PlayerPrefs.SetFloat (GamePreferences.RegionScore1_1, score);
@@ -29,5 +93,10 @@ public static class GamePreferences
 	public static float GetRegionScore1_1()
 	{
 		return PlayerPrefs.GetFloat (GamePreferences.RegionScore1_1);
+	}
+
+	public static void ResetGame()
+	{
+		
 	}
 }
