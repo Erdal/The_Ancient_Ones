a0e7acc6f6ef16dd96261ebe7cedf26c63ca4dd1
Erdal Erkilic <erdal.mybooks@hotmail.com>
2017-01-12 07:55:55 +0800

    So this is amazing, every upgrade has a upgradeButton and a downgradeButton, instead of me creating two methods for every upgrade i created two methods for all of them together. I am to sleep depreived to describe this right now, please look at commented code. The IncreaseLevel method works perfectly for all of them, next i will finish off the DecreaseLevel method.

--- Assets/Resources/Scripts/Controller/UpgradeController.cs
+++ Assets/Resources/Scripts/Controller/UpgradeController.cs
@@ -1,17 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System;
+using System.Reflection;
 
-public class UpgradeController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class UpgradeController : MonoBehaviour 
+{
+	Type prefType;
 	
+	// Use this for initialization
+	void Start () 
+	{
+		SetCompoinents ();
+	}
+
+	void SetCompoinents()
+	{
+		prefType = typeof(GamePreferences); //Get type of class
 	}
 
 	//Go to the World_Map scene
 	public void WorldMap()
 	{
 		SceneManager.LoadScene("World_Map"); //Go to scene
+	}
+
+	//Increase level of selected upgrade
+	public void IncreaseLevel(string objectName)
+	{
+		MethodInfo methodInfoGet = prefType.GetMethod ("Get" + objectName); //Get this method by name
+
+		var tempValueHolder = methodInfoGet.Invoke (null, null); //Call method and chuck the return value into tempValueHolder
+		int tempValue = Convert.ToInt32(tempValueHolder); //Convert tempValueHolder into a int and place in tempValue
+
+		object[] parameters = new object[] { tempValue + 1 }; //Our parameters that we plan to send
+
+		MethodInfo methodInfoSet = prefType.GetMethod ("Set" + objectName); //Get this method by name
+		methodInfoSet.Invoke (null, parameters); //Call method and send parameter with it
+		//Debug.Log(methodInfoGet.Invoke (null, null));
+	}
+
+	//Decrease level of selected upgrade
+	public void DecreaseLevel()
+	{
+		
 	}
 }
