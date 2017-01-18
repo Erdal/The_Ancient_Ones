a15555ef604f30dc8eecb80ce23825c976b7a067
Erdal Erkilic <erdal.mybooks@hotmail.com>
2017-01-18 15:32:24 +0800

    I turned all my code in the Update() method in the SpawnEnemy class into a method system so that i have less code costently playing all the damn time

--- Assets/Resources/Scripts/Enemies/SpawnEnemy.cs
+++ Assets/Resources/Scripts/Enemies/SpawnEnemy.cs
@@ -27,6 +27,7 @@ public class SpawnEnemy : MonoBehaviour
     {
 		SetCompoinents ();
         AddWaves(5, 500); //We want 5 random waves
+		CommitWaves();
     }
 
 	//Set our varable compoinents
@@ -55,37 +56,58 @@ public class SpawnEnemy : MonoBehaviour
         }
     }
 
+	void CommitWaves()
+	{
+		int currentWave = gamePlayManager.Wave; //Get the index of the current wave
+		if (currentWave < waves.Count) //Check if last wave
+		{
+			float timeInterval = Time.time - lastSpawnTime; //calculate how much time passed since the last enemy spawn 
+			float spawnInterval = waves[currentWave]._spawnInterval; //Store spawn interbal time
+			//Here you consider two cases. If it’s the first enemy in the wave, you check whether timeInterval is bigger than timeBetweenWaves.
+			//Otherwise, you check whether timeInterval is bigger than this wave’s spawnInterval.
+			if (enemiesSpawned == 0 && timeInterval > timeBetweenWaves || timeInterval > spawnInterval && enemiesSpawned < waves [currentWave]._maxEnemies) 
+			{
+				lastSpawnTime = Time.time; //Your about to spawn an emeny now so make it equell the current time
+				GameObject newEnemy = (GameObject)Instantiate (waves [currentWave]._enemyPrefab); //creating new enewmy for this wave
+				newEnemy.GetComponent<MoveEnemies> ().waypoints = waypoints; //Sets the correct waypoint to follow for new enemy
+				Animator anim = newEnemy.transform.GetChild (0).GetComponent<Animator> (); //store animator of the sprite of the new enemy
+				anim.SetBool ("Walk", true); //Set walk to true so enemy begins to walk
+				enemiesSpawned++; //Update how many enemys have been spawned
+				StartCoroutine (WaitCommitWaves ());
+			}
+			//enemiesSpawned == 0 && timeInterval > timeBetweenWaves || 
+			//You check the number of enemies on screen. If there are none and it was the last enemy in the wave you spawn the next wave
+			else if (enemiesSpawned == waves [currentWave]._maxEnemies && GameObject.FindGameObjectWithTag ("Enemies") == null) 
+			{
+				gamePlayManager.Wave++; //Increase wave
+				enemiesSpawned = 0; //Set back to 0
+				lastSpawnTime = Time.time; //Set to current time
+				StartCoroutine (WaitCommitWaves ());
+			} 
+			else 
+			{
+				StartCoroutine (WaitCommitWaves());
+			}
+		}
+		else
+		{
+			gameStatusPanel.SetActive (true);
+			GameObject.Find("GameManagerController").GetComponent<GameManagerController>().CheckIfCanLevel(GameObject.Find ("GamePlayController").GetComponent<GamePlayController> ().XpBlood);
+			Time.timeScale = 0;
+			//TODO: Finish this off
+		}
+		Debug.Log ("6");
+	}
+
+	IEnumerator WaitCommitWaves()
+	{
+		yield return new WaitForSeconds(1);
+		CommitWaves ();
+	}
+
     // Update is called once per frame
     void Update()
     {
-        int currentWave = gamePlayManager.Wave; //Get the index of the current wave
-        if (currentWave < waves.Count) //Check if last wave
-        {
-			float timeInterval = Time.time - lastSpawnTime; //calculate how much time passed since the last enemy spawn 
-            float spawnInterval = waves[currentWave]._spawnInterval; //Store spawn interbal time
-            //Here you consider two cases. If it’s the first enemy in the wave, you check whether timeInterval is bigger than timeBetweenWaves.
-            //Otherwise, you check whether timeInterval is bigger than this wave’s spawnInterval. 
-            if (((enemiesSpawned == 0 && timeInterval > timeBetweenWaves) || timeInterval > spawnInterval) && enemiesSpawned < waves[currentWave]._maxEnemies)
-            {
-                lastSpawnTime = Time.time; //Your about to spawn an emeny now so make it equell the current time
-                GameObject newEnemy = (GameObject)Instantiate(waves[currentWave]._enemyPrefab); //creating new enewmy for this wave
-                newEnemy.GetComponent<MoveEnemies>().waypoints = waypoints; //Sets the correct waypoint to follow for new enemy
-                Animator anim = newEnemy.transform.GetChild(0).GetComponent<Animator>(); //store animator of the sprite of the new enemy
-                anim.SetBool("Walk", true); //Set walk to true so enemy begins to walk
-                enemiesSpawned++; //Update how many enemys have been spawned
-            }
-            //You check the number of enemies on screen. If there are none and it was the last enemy in the wave you spawn the next wave
-            if (enemiesSpawned == waves[currentWave]._maxEnemies && GameObject.FindGameObjectWithTag("Enemies") == null)
-            {
-                gamePlayManager.Wave++; //Increase wave
-                enemiesSpawned = 0; //Set back to 0
-                lastSpawnTime = Time.time; //Set to current time
-            }
-        }
-        else
-        {
-			gameStatusPanel.SetActive (true);
-            //TODO: Finish this off
-        }
+
     }
 }
